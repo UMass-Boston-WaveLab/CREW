b904ddc84a29ff93285f83561be2eac9490c699e
Eric Brown <brown.eric.m@gmail.com>
2015-01-06 17:45:13 -0500

    Readability improvements
    Added sections and bookeeping

--- PL_Security_Sim.m
+++ PL_Security_Sim.m
@@ -1,4 +1,5 @@
 %UMass Boston Physical Layer Security Channel Model
+%Authors: Eric Brown, Clara Gamboa, Dr. K.C. Kirby-Patel
 %
 %--------------------------------------------
 
@@ -9,36 +10,40 @@
 %% This is where we define the variables
 S = 10; % # of Scatterers
 N = 10; % # of Eve samples
-d = .1 % wavelength 
-q = 20 % Number of samples ahead we attempt to predict
-Lamb = 1 
+d = .1; % wavelength 
+q = 20; % Number of samples ahead we attempt to predict
+Lamb = 1; 
 %%
-phiAS = 2*pi*rand(S, 1) %where S is each scatterer (creates a vertical 
+%-----------------------------------------------------
+% In this section we define our wave characteristics. Phase angles and
+% Thetas
+%-----------------------------------------------------
+
+phiAS = 2*pi*rand(S, 1); %where S is each scatterer (creates a vertical 
                         % array with unique angles for each value in the
                        % array)
-
-phiSE = 2*pi*rand(S, 1)                       
+phiSE = 2*pi*rand(S, 1);                       
 
 
 ThetaSE = 2*pi*rand(S, 1); %for each angle required. !!Needs association , S = scat
 
 ThetaAS = 2*pi*rand(S, 1);
-
+%%
+%unused
 %--------------------------
 %PSA = exp(j*(phiAS) + (phiSE)) %multiplies phasor values 
   %by exponent and imag 
 %PSB = exp(j*(phiBS) + (phiSE))
 %--------------------------
-
+%%
 % d is specified in terms of lambda, therefore, lambda = 1
 % This defines the point on the wave as determined by distance from center
 % of E 
 %This is the phase term not including any doppler shenanigans
-ArrPhase = repmat(phiSE, [1, N+q]) + repmat(phiAS, [1, N+q]) + kron(((2*pi)/Lamb)*d*((1:(N+q))-(N+1)/2),cos(ThetaSE))
+ArrPhase = repmat(phiSE, [1, N+q]) + repmat(phiAS, [1, N+q]) + kron(((2*pi)/Lamb)*d*((1:(N+q))-(N+1)/2),cos(ThetaSE));
 
 ArrScatCont = exp(j*ArrPhase);
 
+sum(ArrScatCont, 1);
 %AS = sum(ALL AS) %places phasors into array !!Not sure how this is going to work!!
-
- 
 %BS = sum(ALL BS)
