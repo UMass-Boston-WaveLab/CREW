51f66fa30c1435585b7b8f82518baaeb36abb338
Eric Brown <brown.eric.m@gmail.com>
2015-02-04 16:00:56 -0500

    Yay it somewhat works
    The predict function has been modified to account for some oddities in
    the lpc function.

--- predict.m
+++ predict.m
@@ -1,5 +1,7 @@
-
-function [new_vec] = predict(a,x)
-x_hat = dot(a,x);
-new_vec = [x(2:end) x_hat];
+%% This is a new function called predict. 
+% It allows you to create a new vector of x_hat (predicted future values)
+function [x_hat] = predict(a,x)
+x_hat = dot(-a(2:end),fliplr(x((length(x)-length(a)+2):end)));
+%new_vec = [x x_hat];
 end
+%%
