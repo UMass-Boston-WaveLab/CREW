8e7104987ba9301b53431ee0965103b7fc6e2d0b
kckerbypatel <kc.kerby-patel@umb.edu>
2015-06-10 15:06:45 -0400

    CRLB program rev 0 completed
    Untested draft check-in

--- CRLBmontecarlo.m
+++ CRLBmontecarlo.m
@@ -20,21 +20,24 @@ M=100;
 N=10;
 d=0.1;
 q=floor(1/d);
+SNR=10; %in dB: 10*log10(avg(|h|^2/|n|^2)) = 10*log10(N/sigma^2)
 
 testM = 10:10:300;
 testN = 1:100;
 testd = 0.05:0.05:0.5;
 testq = floor(0.25/d):floor(0.25/d):floor(10/d);
+testSNR=1:30; %in dB
 
 temp=0;
 CRLBvsM = zeros(size(testM));
 CRLBvsN = zeros(size(testN));
 CRLBvsd = zeros(size(testd));
-CRLBvsq = zeros(size(textq));
+CRLBvsq = zeros(size(testq));
+CRLBvsSNR = zeros(size(testSNR));
 
 for ii=1:length(testN)
     for jj = 1:reps
-        temp=temp+chanestCRLB(testN(ii), M, d, q)/reps;
+        temp=temp+chanestCRLB(testN(ii), M, d, q, 10^(SNR/10))/reps; %averaging
     end
     CRLBvsN(ii)=temp;
     temp=0;
@@ -42,7 +45,7 @@ end
 
 for ii=1:length(testM)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, testM(ii), d, q)/reps;
+        temp=temp+chanestCRLB(N, testM(ii), d, q, 10^(SNR/10))/reps;
     end
     CRLBvsM(ii)=temp;
     temp=0;
@@ -50,7 +53,7 @@ end
 
 for ii=1:length(testd)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, M, testd(ii), q)/reps;
+        temp=temp+chanestCRLB(N, M, testd(ii), q, 10^(SNR/10))/reps;
     end
     CRLBvsd(ii)=temp;
     temp=0;
@@ -58,9 +61,17 @@ end
 
 for ii=1:length(testq)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, M, d, testq(ii))/reps;
+        temp=temp+chanestCRLB(N, M, d, testq(ii), 10^(SNR/10))/reps;
     end
     CRLBvsq(ii)=temp;
+    temp=0;
+end
+
+for ii=1:length(testq)
+    for jj = 1:reps
+        temp=temp+chanestCRLB(N, M, d, q, 10^(testSNR(ii)/10))/reps; 
+    end
+    CRLBvsSNR(ii)=temp;
     temp=0;
 end
 
--- chanestCRLB.m
+++ chanestCRLB.m
@@ -1,4 +1,4 @@
-function [ CRLB ] = chanestCRLB( N, M, d, q)
+function [ CRLB ] = chanestCRLB( N, M, d, q, SNR)
 %CHANESTCRLB Computes the Cramer-Rao Lower Bound on the variance of an
 %unbiased estimator of the channel transfer function at one location based on
 %M spatial samples.
@@ -6,14 +6,48 @@ function [ CRLB ] = chanestCRLB( N, M, d, q)
 %   Not worried about estimating how many scatterers there are - that is a
 %   separate problem
 %   d (separation between samples) is specified in wavelengths
-%   CRLB depends on k, alpha, which are random variables.  We can assume a
-%   distribution for them and then calculate the actual expected value (this is
-%   probably more general/correct) or we can Monte Carlo it.
-%   We're going to do it the Monte Carlo way, since that's what Svantesson
-%   and Swindlehurst do in their MIMO CRLB paper.
+%   CRLB depends on k, alpha, which are random variables.  This computes
+%   the CRLB for one particular channel realization - it is called by a
+%   wrapper program that runs a Monte Carlo simulation to get an average
+%   value.
 
+%%generate random values that we can use to construct the channel: alpha_n,
+%theta_n, psi_n, k_n
+theta = 2*pi*rand(1, N);
+psi = 2*pi*rand(1,N);
+alpha = (1/sqrt(2))*(randn(1,N)+1i*randn(1,N)); %randn has variance 1, so 
+                        %E(alpha^2) is 1 if alpha is constructed this way
 
+k = 2*pi*(1+(1e-8)*cos(theta)).*cos(psi);
 
+%since SNR = N/sigma^2, sigma^2 = N/SNR.
+sigma = sqrt(N/SNR); %we never use sigma not-squared, but be consistent w/notation
 
+%%create Hprime, the derivatives of h wrt parameters
+Hprime = [0 exp(1i*k*q*d) 1i*exp(1i*k*q*d) 1i*alpha.*d*q.*exp(1i*k*q*d)];
+
+%%create B, the CRLB for the parameters
+%Binv is made of different combinations of DR, DI, and Dk except for
+%Binv(1,1).
+DR = zeros(M,N);
+DRrow = exp(1i*k*d); %k is a row vector
+for ii = 1:M
+    DR(ii,:) = DRrow.^ii;
+end
+
+DI = 1i*DR;
+
+Dk = zeros(M,N);
+for ii=1:M
+    Dk(ii,:) = 1i*ii*d*alpha.*DRrow.^ii;
+end
+
+Binv = [M/sigma^4 zeros(1, 3*N);
+        zeros(N,1) 2/(sigma^2)*real(DR'*DR) 2/(sigma^2)*real(DR'*DI) 2/(sigma^2)*real(DR'*Dk);
+        zeros(N,1) 2/(sigma^2)*real(DI'*DR) 2/(sigma^2)*real(DI'*DI) 2/(sigma^2)*real(DI'*Dk);
+        zeros(N,1) 2/(sigma^2)*real(Dk'*DR) 2/(sigma^2)*real(Dk'*DI) 2/(sigma^2)*real(Dk'*Dk)];
+
+CRLB=Hprime*(Binv\Hprime');
+    
 end
 
