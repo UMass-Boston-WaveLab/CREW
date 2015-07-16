3571dcc1699d1eb7f18f71fea0f9414ea6b6864d
kckerbypatel <kc.kerby-patel@umb.edu>
2015-07-16 16:15:24 -0400

    mostly fixed CRLB program
    Corrected CRLB math simplification, improved implementation in the
    program - chanestCRLB2 now doesn’t have a singular information matrix
    updated Educated Eavesdropping summary (sent to NSF PM)
    More detail about limits of our analysis because of the assumptions we
    make

--- CRLBmontecarlo.m
+++ CRLBmontecarlo.m
@@ -1,4 +1,4 @@
-function [CRLBvsM, CRLBvsN, CRLBvsd, CRLBvsq, CRLBvsSNR] = CRLBmontecarlo(reps)
+function [CRLBvsL, CRLBvsN, CRLBvsd, CRLBvsq, CRLBvsSNR] = CRLBmontecarlo(reps)
 %CRLBMONTECARLO Uses the Monte Carlo method to estimate the Cramer-Rao 
 %Lower Bound on the variance of an unbiased estimator of the channel 
 %transfer function at one location based on M spatial samples at other 
@@ -16,20 +16,20 @@ function [CRLBvsM, CRLBvsN, CRLBvsd, CRLBvsq, CRLBvsSNR] = CRLBmontecarlo(reps)
 %   parameters.
 
 %default values
-M=100;
+L=20;
 N=10;
-d=0.5;
+d=0.1;
 SNR=20; %in dB: 10*log10(avg(|h|^2/|n|^2)) = 10*log10(N/sigma^2)
-q=M+2/d;
+q=floor(L/d)+2/d; %2 wavelengths beyond sample region is default
 
-testM = 85:10:300;
+testL = 12:2:200;
 testN = 1:30;
 testd = 0.05:0.05:0.5;
-testq = floor(M/2):1:(M+floor(5/d));
+testq = floor(0.5*L/d):1:(floor(L/d)+floor(5/d));
 testSNR=13:30; %in dB
 
 temp=0;
-CRLBvsM = zeros(size(testM));
+CRLBvsL = zeros(size(testL));
 CRLBvsN = zeros(size(testN));
 CRLBvsd = zeros(size(testd));
 CRLBvsq = zeros(size(testq));
@@ -37,17 +37,17 @@ CRLBvsSNR = zeros(size(testSNR));
 
 
 
-for ii=1:length(testM)
+for ii=1:length(testL)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, testM(ii), d, q, 10^(SNR/10))/reps;
+        temp=temp+chanestCRLB2(N, testL(ii), d, q, 10^(SNR/10))/reps;
     end
-    CRLBvsM(ii)=temp;
+    CRLBvsL(ii)=temp;
     temp=0;
 end
 
 for ii=1:length(testN)
     for jj = 1:reps
-        temp=temp+chanestCRLB(testN(ii), M, d, q, 10^(SNR/10))/reps; %averaging
+        temp=temp+chanestCRLB2(testN(ii), L, d, q, 10^(SNR/10))/reps; %averaging
     end
     CRLBvsN(ii)=temp;
     temp=0;
@@ -55,7 +55,7 @@ end
 
 for ii=1:length(testd)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, M, testd(ii), q, 10^(SNR/10))/reps;
+        temp=temp+chanestCRLB2(N, L, testd(ii), q, 10^(SNR/10))/reps;
     end
     CRLBvsd(ii)=temp;
     temp=0;
@@ -63,7 +63,7 @@ end
 
 for ii=1:length(testq)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, M, d, testq(ii), 10^(SNR/10))/reps;
+        temp=temp+chanestCRLB2(N, L, d, testq(ii), 10^(SNR/10))/reps;
     end
     CRLBvsq(ii)=temp;
     temp=0;
@@ -71,34 +71,39 @@ end
 
 for ii=1:length(testSNR)
     for jj = 1:reps
-        temp=temp+chanestCRLB(N, M, d, q, 10^(testSNR(ii)/10))/reps; 
+        temp=temp+chanestCRLB2(N, L, d, q, 10^(testSNR(ii)/10))/reps; 
     end
     CRLBvsSNR(ii)=temp;
     temp=0;
 end
 figure; 
-plot(testM, real(CRLBvsM))
-xlabel('Number of Measurements')
+plot(testL, real(CRLBvsL))
+xlabel('Measurement Length (wavelengths)')
 ylabel('Estimator Minimum Variance')
+ylim([0,1])
 
 figure; 
 plot(testN, real(CRLBvsN))
 xlabel('Number of Scatterers')
 ylabel('Estimator Minimum Variance')
+ylim([0,1])
 
 figure; 
 plot(testd, real(CRLBvsd))
 xlabel('Space Between Samples (Wavelengths)')
 ylabel('Estimator Minimum Variance')
+ylim([0,1])
 
 figure; 
-plot(testq-M, real(CRLBvsq))
+plot(testq-floor(L/d), real(CRLBvsq))
 xlabel(sprintf('Number of Samples Predicted Ahead (sample spacing = %.2f wavelengths)', d))
 ylabel('Estimator Minimum Variance')
+ylim([0,1])
 
 figure; 
 plot(testSNR, real(CRLBvsSNR))
 xlabel('Signal to Noise Ratio (dB)')
 ylabel('Estimator Minimum Variance')
+ylim([0,1])
 
 end
