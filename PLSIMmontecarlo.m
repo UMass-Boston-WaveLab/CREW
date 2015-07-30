afd8a786980a78347492e0b7125d65698ee67bdb
kckerbypatel <kc.kerby-patel@umb.edu>
2015-07-30 14:14:21 -0400

    add test for failed "find"

--- PLSIMmontecarlo.m
+++ PLSIMmontecarlo.m
@@ -8,13 +8,13 @@ function [PLSIMvsS, PLSIMvsN, PLSIMvsd, PLSIMvsP, PLSIMvsSNR,PLSIMvsf_d,PLSIMvsf
 
 %default values
 S    = 6;                % # of Scatterers
-N    = 200;               % # of sensor array samples
+N    = 100;               % # of sensor array samples
 d    = 0.1;              % Spacing between eavesdropper samples in wavelengths
-q    = 200;               % Number of samples ahead we attempt to predict
+q    = 50;               % Number of samples ahead we attempt to predict
 P    = 30;                % Number of complex sinusoids that make up the wireless channel
 f_d  = 11000;             % doppler frequency
 f_c  = 2400000;           % carrier frequency
-SNR = 20;                   % Signal to Noise Ratio in dB.
+SNR = 200;                   % Signal to Noise Ratio in dB.
 
 %test ranges
 testS = 1:20;
@@ -40,19 +40,34 @@ for kk = 1:reps
   
         [H, H_hat] = PL_Security_Sim_pmusic(testS(ii), N, d, P,SNR); %tests the scenario
         err = abs((H-H_hat)/rms(H)); % finds error percentage
-        q_maxS(ii) = q_maxS(ii)+find(err>0.05, 1)/reps;
+        try
+            temp=find(err>0.05, 1);
+        catch
+            temp=length(H);
+        end
+        q_maxS(ii) = q_maxS(ii)+temp/reps;
     end
     
     for ii=1:length(testN)
         [H, H_hat] = PL_Security_Sim_pmusic(S, testN(ii), d, P,SNR); %tests the scenario
         err = abs((H-H_hat)/rms(H)); % finds error percentage
-        q_maxN(ii) = q_maxN(ii) + find(err>0.05, 1)/reps;
+        try
+            temp=find(err>0.05, 1);
+        catch
+            temp=length(H);
+        end
+        q_maxN(ii) = q_maxN(ii) + temp/reps;
     end
     
     for ii=1:length(testd)
         [H, H_hat] = PL_Security_Sim_pmusic(S, N, testd(ii), P,SNR); %tests the scenario
         err = abs((H-H_hat)/rms(H)); % finds error percentage
-        q_maxd(ii) = q_maxd(ii) + find(err>0.05, 1)/reps;
+        try
+            temp=find(err>0.05, 1);
+        catch
+            temp=length(H);
+        end
+        q_maxd(ii) = q_maxd(ii) + temp/reps;
     end
     
 %    for ii=1:length(testq)
@@ -64,7 +79,12 @@ for kk = 1:reps
     for ii=1:length(testP)
         [H, H_hat] = PL_Security_Sim_pmusic(S, N, d, testP(ii),SNR); %tests the scenario
         err = abs((H-H_hat)/rms(H)); % finds error percentage
-        q_maxP(ii) = q_maxP(ii) + find(err>0.05, 1)/reps;
+        try
+            temp=find(err>0.05, 1);
+        catch
+            temp=length(H);
+        end
+        q_maxP(ii) = q_maxP(ii) + temp/reps;
     end
     
 %     for ii=1:length(testf_d)
@@ -81,7 +101,12 @@ for kk = 1:reps
     for ii=1:length(testSNR)
         [H, H_hat] = PL_Security_Sim_pmusic(S, N, d, P,testSNR(ii)); %tests the scenario
         err = abs((H-H_hat)/rms(H)); % finds error percentage
-        q_maxSNR(ii) = q_maxSNR(ii) + find(err>0.05, 1)/reps;
+        try
+            temp=find(err>0.05, 1);
+        catch
+            temp=length(H);
+        end
+        q_maxSNR(ii) = q_maxSNR(ii) + temp/reps;
     end
 end
 figure; 
