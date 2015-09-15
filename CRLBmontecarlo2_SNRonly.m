8ef64f1a364c421a3f01de868266351f5caa415d
kckerbypatel <kc.kerby-patel@umb.edu>
2015-09-15 14:20:50 -0400

    edits to CRLB program
    Made graph lines wider
    Added program to only make the SNR plot, because that’s what I needed
    right then.

--- CRLBmontecarlo2_SNRonly.m
+++ CRLBmontecarlo2_SNRonly.m
@@ -1,4 +1,4 @@
-function [CRLBvsM, CRLBvsN, CRLBvsd, CRLBvsq, CRLBvsSNR] = CRLBmontecarlo2(reps)
+function [CRLBvsM, CRLBvsN, CRLBvsd, CRLBvsq, CRLBvsSNR] = CRLBmontecarlo2_SNRonly(reps)
 %CRLBMONTECARLO Uses the Monte Carlo method to estimate the Cramer-Rao 
 %Lower Bound on the variance of an unbiased estimator of the channel 
 %transfer function at one location based on M spatial samples at other 
@@ -46,53 +46,53 @@ rvsq = zeros(size(testq));
 rvsSNR = zeros(size(testSNR));
 rvsdk = zeros(size(testdk));
 
-parfor ii=1:length(testM)
-    temp=0;
-    for jj = 1:reps
-        [r, crlb]=chanestCRLB(N, testM(ii), d, q, 10^(SNR/10), mindk);
-        temp=temp+crlb/reps;
-        rvsM(ii) = rvsM(ii)+r/reps;
-    end
-    CRLBvsM(ii)=temp;
-    
-    
-end
-
-parfor ii=1:length(testN)
-    temp=0;
-    for jj = 1:reps
-        [r, crlb]=chanestCRLB(testN(ii), M, d, q, 10^(SNR/10), mindk); %averaging
-        temp=temp+crlb/reps;
-        rvsN(ii) =rvsN(ii)+r/reps;
-    end
-    CRLBvsN(ii)=temp;
-    
-    
-end
-
-parfor ii=1:length(testd)
-    temp=0;
-    for jj = 1:reps
-        [r, crlb]=chanestCRLB(N, M*d/testd(ii), testd(ii), q, 10^(SNR/10), mindk);
-        temp=temp+crlb/reps;
-         rvsd(ii) = rvsd(ii)+r/reps;
-    end
-    CRLBvsd(ii)=temp;
-    
-   
-end
-
-parfor ii=1:length(testq)
-    temp=0;
-    for jj = 1:reps
-        [r, crlb]=chanestCRLB(N, M, d, testq(ii), 10^(SNR/10), mindk);
-        temp=temp+crlb/reps;
-        rvsq(ii) = rvsq(ii)+r/reps;
-    end
-    CRLBvsq(ii)=temp;
-    
-    
-end
+% parfor ii=1:length(testM)
+%     temp=0;
+%     for jj = 1:reps
+%         [r, crlb]=chanestCRLB(N, testM(ii), d, q, 10^(SNR/10), mindk);
+%         temp=temp+crlb/reps;
+%         rvsM(ii) = rvsM(ii)+r/reps;
+%     end
+%     CRLBvsM(ii)=temp;
+%     
+%     
+% end
+% 
+% parfor ii=1:length(testN)
+%     temp=0;
+%     for jj = 1:reps
+%         [r, crlb]=chanestCRLB(testN(ii), M, d, q, 10^(SNR/10), mindk); %averaging
+%         temp=temp+crlb/reps;
+%         rvsN(ii) =rvsN(ii)+r/reps;
+%     end
+%     CRLBvsN(ii)=temp;
+%     
+%     
+% end
+% 
+% parfor ii=1:length(testd)
+%     temp=0;
+%     for jj = 1:reps
+%         [r, crlb]=chanestCRLB(N, M*d/testd(ii), testd(ii), q, 10^(SNR/10), mindk);
+%         temp=temp+crlb/reps;
+%          rvsd(ii) = rvsd(ii)+r/reps;
+%     end
+%     CRLBvsd(ii)=temp;
+%     
+%    
+% end
+% 
+% parfor ii=1:length(testq)
+%     temp=0;
+%     for jj = 1:reps
+%         [r, crlb]=chanestCRLB(N, M, d, testq(ii), 10^(SNR/10), mindk);
+%         temp=temp+crlb/reps;
+%         rvsq(ii) = rvsq(ii)+r/reps;
+%     end
+%     CRLBvsq(ii)=temp;
+%     
+%     
+% end
 
 parfor ii=1:length(testSNR)
     temp=0;
@@ -106,17 +106,17 @@ parfor ii=1:length(testSNR)
     
 end
 
-parfor ii=1:length(testdk)
-    temp=0;
-    for jj = 1:reps
-        [r, crlb]=chanestCRLB(N, M, d, q, 10^(SNR/10), testdk(ii));
-        temp=temp+crlb/reps; 
-        rvsdk(ii) = rvsdk(ii)+r/reps;
-    end
-    CRLBvsdk(ii)=temp;
-    
-    
-end
+% parfor ii=1:length(testdk)
+%     temp=0;
+%     for jj = 1:reps
+%         [r, crlb]=chanestCRLB(N, M, d, q, 10^(SNR/10), testdk(ii));
+%         temp=temp+crlb/reps; 
+%         rvsdk(ii) = rvsdk(ii)+r/reps;
+%     end
+%     CRLBvsdk(ii)=temp;
+%     
+%     
+% end
 
 %these are for debugging the default parameter choices
 % figure;
@@ -135,33 +135,33 @@ end
 % ylabel('Info Matrix Avg Rank')
 
 
-h1=figure; 
-plot(testM*d, real(CRLBvsM), 'linewidth',2)
-xlabel('Measurement Length (wavelengths)')
-ylabel('Estimator Minimum Variance')
-ylim([0,0.1])
-narrowfig(h1)
-
-h2=figure; 
-plot(testN, real(CRLBvsN), 'linewidth',2)
-xlabel('Number of Scatterers')
-ylabel('Estimator Minimum Variance')
-ylim([0,0.1])
-narrowfig(h2)
-
-h3=figure; 
-plot(testd, real(CRLBvsd), 'linewidth',2)
-xlabel('Space Between Samples (Wavelengths) - constant total length')
-ylabel('Estimator Minimum Variance')
-ylim([0,0.1])
-narrowfig(h3)
-
-h4=figure; 
-plot(testq-M, real(CRLBvsq), 'linewidth',2)
-xlabel(sprintf('Number of Samples Predicted Ahead (sample spacing = %.2f wavelengths)', d))
-ylabel('Estimator Minimum Variance')
-ylim([0,0.1])
-narrowfig(h4)
+% h1=figure; 
+% plot(testM*d, real(CRLBvsM), 'linewidth',2)
+% xlabel('Measurement Length (wavelengths)')
+% ylabel('Estimator Minimum Variance')
+% ylim([0,0.1])
+% narrowfig(h1)
+% 
+% h2=figure; 
+% plot(testN, real(CRLBvsN), 'linewidth',2)
+% xlabel('Number of Scatterers')
+% ylabel('Estimator Minimum Variance')
+% ylim([0,0.1])
+% narrowfig(h2)
+% 
+% h3=figure; 
+% plot(testd, real(CRLBvsd), 'linewidth',2)
+% xlabel('Space Between Samples (Wavelengths) - constant total length')
+% ylabel('Estimator Minimum Variance')
+% ylim([0,0.1])
+% narrowfig(h3)
+% 
+% h4=figure; 
+% plot(testq-M, real(CRLBvsq), 'linewidth',2)
+% xlabel(sprintf('Number of Samples Predicted Ahead (sample spacing = %.2f wavelengths)', d))
+% ylabel('Estimator Minimum Variance')
+% ylim([0,0.1])
+% narrowfig(h4)
 
 h5=figure; 
 plot(testSNR, real(CRLBvsSNR), 'linewidth',2)
@@ -170,14 +170,14 @@ ylabel('Estimator Minimum Variance')
 ylim([0,0.1])
 narrowfig(h5)
 
-h6=figure; 
-plot(testdk*M*d, real(CRLBvsdk), 'linewidth',2)
-xlabel('Minimum \Delta_kMd (radians)') %total accumulated phase difference btw. two scatterer paths over array length
-ylabel('Estimator Minimum Variance')
-set(gca, 'xtick', [0, pi/8, pi/4,3*pi/8, pi/2])
-set(gca, 'xticklabel', {'0';'\pi/8';'\pi/4'; '3\pi/8'; '\pi/2'})
-ylim([0,0.1])
-narrowfig(h6)
+% h6=figure; 
+% plot(testdk*M*d, real(CRLBvsdk), 'linewidth',2)
+% xlabel('Minimum \Delta_kMd (radians)') %total accumulated phase difference btw. two scatterer paths over array length
+% ylabel('Estimator Minimum Variance')
+% set(gca, 'xtick', [0, pi/8, pi/4,3*pi/8, pi/2])
+% set(gca, 'xticklabel', {'0';'\pi/8';'\pi/4'; '3\pi/8'; '\pi/2'})
+% ylim([0,0.1])
+% narrowfig(h6)
 
 % figure; 
 % plot(testdk*M*d, rvsdk)
